b8cd6bec811e7c8cfbe40d72cff7fe8f1348959a
carolelias <105891351+carolelias@users.noreply.github.com>
2023-12-17 19:39:38 -0300

    adicionando funcionalidade 12 no .h

--- headers/funcionalidades.h
+++ headers/funcionalidades.h
@@ -5,11 +5,14 @@
 // funcionalidade 8
 void geraGrafo(char *arq1);
 
-//funcionalidade 9
+// funcionalidade 9
 void geraGrafoTransposto(char *arq1);
 
-//funcionalidade 10
+// funcionalidade 10
 void pesquisaGrafo(char *arq1, int n);
 
-//funcionalidade 11
+// funcionalidade 11
 void conexoGrafo(char *arq1);
+
+// funcionalidade 12
+void caminhoMaisCurto(char *arq1)
